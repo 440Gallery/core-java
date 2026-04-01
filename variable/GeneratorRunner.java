d0f1f8c6e8d267e9bebf036d00e9b3d641db62b4
unknown <btanusha993@gmail.com>
2026-04-01 09:26:20 +0530

    update

--- variable/GeneratorRunner.java
+++ variable/GeneratorRunner.java
@@ -32,5 +32,33 @@ class GeneratorRunner {
         System.out.println("Control Panel Type: " + gen1.controlPanelType);
         System.out.println("Dimensions: " + gen1.dimensionsCM + " cm");
         System.out.println("Safety Features: " + gen1.safetyFeatures);
+		
+		System.out.println("\nBrand: " + gen2.brand);
+        System.out.println("Model: " + gen2.model);
+        System.out.println("Type: " + gen2.type);
+        System.out.println("Power Output: " + gen2.powerOutputKW + " kW");
+        System.out.println("Fuel Type: " + gen2.fuelType);
+        System.out.println("Fuel Capacity: " + gen2.fuelCapacityLiters + " L");
+        System.out.println("Runtime Hours: " + gen2.runtimeHours);
+        System.out.println("Portable: " + gen2.portable);
+        System.out.println("Weight: " + gen2.weight + " kg");
+        System.out.println("Color: " + gen2.color);
+        System.out.println("Price: ₹" + gen2.price);
+        System.out.println("Warranty Months: " + gen2.warrantyMonths);
+        System.out.println("Manufacturer: " + gen2.manufacturer);
+        System.out.println("Country of Origin: " + gen2.countryOfOrigin);
+        System.out.println("Inverter Technology: " + gen2.inverterTechnology);
+        System.out.println("Cooling System: " + gen2.coolingSystem);
+        System.out.println("Noise Level: " + gen2.noiseLevelDB + " dB");
+        System.out.println("Start Type: " + gen2.startType);
+        System.out.println("Automatic Transfer Switch: " + gen2.automaticTransferSwitch);
+        System.out.println("Barcode: " + gen2.barcode);
+        System.out.println("Discount: " + (gen2.discount * 100) + "%");
+        System.out.println("Quantity: " + gen2.quantity);
+        System.out.println("Material: " + gen2.material);
+        System.out.println("Eco Mode: " + gen2.ecoMode);
+        System.out.println("Control Panel Type: " + gen2.controlPanelType);
+        System.out.println("Dimensions: " + gen2.dimensionsCM + " cm");
+        System.out.println("Safety Features: " + gen2.safetyFeatures);
     }
 }
